5f67e6cfbaa5a98b2e1e4083c26e787dc44dd9d2
H. F. Bassani <hfb@cin.ufpe.br>
2018-05-31 17:27:57 -0300

    - Pequena mudança na função de reward para fazer o robô não ficar parado - Atualização do VSS-Simulator

--- utils/options.py
+++ utils/options.py
@@ -32,8 +32,8 @@ class Params(object):   # NOTE: shared across all modules
         self.verbose     = 0            # 0(warning) | 1(info) | 2(debug)
 
         # training signature
-        self.machine     = "Cin"    # "machine_id"
-        self.timestamp   = "180521"   # "yymmdd##"
+        self.machine     = "Macbook"    # "machine_id"
+        self.timestamp   = "reward2"   # "yymmdd##"
         # training configuration
         self.mode        = 1            # 1(train) | 2(test model_file)
         self.config      = 9
